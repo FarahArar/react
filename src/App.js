f31acdc609920cb1d7bb662849320356f100d5b0
Farah Arar <farah02arar@gmail.com>
2024-09-26 13:56:47 +0300

    added modal and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
+import Employee from './components/Employee.js';
+import {v4 as uuidv4} from 'uuid'
 
 function App() {
+  const [role, setRole] = useState('default');
+  const [employees, setEmployee] =useState(
+    [
+      {id:1, name: "Caleb", role: 'Senior', img:"https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg?auto=compress&cs=tinysrgb&w=600"},
+      {id:2, name: "Emily", role: 'Intern', img:"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=400"},
+      {id:3, name: "Camilla", role: 'developer', img:"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg?auto=compress&cs=tinysrgb&w=400"},
+      {id:4, name: "John", role: 'manager', img:"https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=400"},
+
+    ]
+  )
+
+  const showEmployee = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="app"> 
+      <input type='text' onChange={(e)=>
+        setRole(e.target.value)
+      }/>
+    {showEmployee?(
+
+      <div className='flex flex-wrap justify-center'>
+        { employees.map((employee) => {
+          console.log(uuidv4())
+          return(
+        <Employee
+            key={uuidv4()}
+            name={employee.name}
+            role={employee.role}
+            img={employee.img}
+        />
+          )
+        })}
+        
+
+      </div>
+      ): (
+      <p>you cannot view the employees</p>
+      )}
     </div>
   );
 }
